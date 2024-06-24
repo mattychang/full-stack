99c360a195366b1a8106fac921b5fcbea0426038
Matthew Chang <157415528+mattychang@users.noreply.github.com>
2024-06-24 16:42:38 -0400

    Create main.js

---
+++ Project 2/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import TaskManager from './TaskManager.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+createApp(TaskManager).mount('#app');

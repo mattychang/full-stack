dfffbbf2791d54b8aeb97d7363b80b0511800025
Matthew Chang <157415528+mattychang@users.noreply.github.com>
2024-06-27 16:25:02 -0400

    Create App.tsx

---
+++ Project 3/App.tsx
@@ -0,0 +1,17 @@
+// Matthew Chang
+// CSDS 221: launch app
+
+// imports
+import React from 'react';
+import Hangman from './Hangman';
+
+// functional component defintion
+function App() {
+  return (
+    <div className = "App">
+      <Hangman />
+    </div>
+  );
+}
+
+export default App;

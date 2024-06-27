65edec8b25cb51e5960ca676069ed899bb63f3ee
Matthew Chang <157415528+mattychang@users.noreply.github.com>
2024-06-27 16:24:41 -0400

    Create List.tsx

---
+++ Project 3/List.tsx
@@ -0,0 +1,30 @@
+// Matthew Chang
+// CSDS 221 : list of words and associated hints
+
+interface WordHint {
+  word: string;
+  hint: string;
+}
+
+export const menWordsList: WordHint[] = [
+  { word: "adamondra", hint: "Czech, 31, First to send 'Silence' 9c (5.15d)" },
+  { word: "chrissharma", hint: "American, 39, Pioneered deep-water soloing and hard sport climbs" },
+  { word: "alexhonnold", hint: "American, 35, Free solo of El Capitan" },
+  { word: "tommycaldwell", hint: "American, 42, First free ascent of the Dawn Wall" },
+  { word: "danielwoods", hint: "American, 31, The definition of a boulder bro" },
+  { word: "magnusmidtbo", hint: "Norway, 36, My favorite YouTuber lol" },
+  { word: "tomoanarasaki", hint: "Japanese, 27, The definition of being dynamic" },
+  { word: "willbosi", hint: "British, 25, Currently has sent the most V17s" },
+  { word: "tobyroberts", hint: "British, 18, Upcoming competition star" },
+  { word: "soratoanraku", hint: "Japanese, 17, Dominant compeition climber" }
+];
+
+
+export const womenWordsList: WordHint[] = [
+  { word: "janjagarnbret", hint: "Slovenian, 25, The most dominant female climber of all time" },
+  { word: "aimori", hint: "Japanese, 20, An exceptional talent for her size" },
+  { word: "chaehyunseo", hint: "South Korean, 20, Excellent competition climber" },
+  { word: "brookeraboutou", hint: "American, 23, Incredibly versatile climber with the most iconic brother" },
+  { word: "nataliagrossman", hint: "American, 23, A powerful and dynamic climber" }
+];
+
